71f4f3b8c420dd14471572c6896940d71757ac83
Coding_ManojP <57979895+CodingManoj@users.noreply.github.com>
2024-07-27 14:07:32 -0400

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export ANSIBLE_DEPRECATION_WARNINGS=False
+export ANSIBLE_FORCE_COLOR=1
+export HISTTIMEFORMAT="%d/%m/%y %T "
+export AWS_PAGER=""
+export PROMPT_COMMAND='$(history -w)'
+export HISTSIZE=10000
+export SYSTEMD_PAGER=cat
